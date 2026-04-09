65d46fbe3b94c0e8bfdec4971760265df302e328
Evggeenn <ionov.z@bk.ru>
2026-04-09 23:03:03 +0300

    add

--- neostore/certificate/forms.py
+++ neostore/certificate/forms.py
@@ -1,8 +1,17 @@
+from django import forms
 from django.forms import ModelForm, TextInput, FileInput
 from home.models import Certificate
 
 
 class CertificateForm(ModelForm):
+    def clean_photo(self):
+        photo = self.cleaned_data.get('photo')
+        if photo:
+            allowed_types = ['image/jpeg', 'image/png', 'image/gif', 'application/pdf']
+            if photo.content_type not in allowed_types:
+                raise forms.ValidationError('Разрешены только изображения (JPG, PNG, GIF) и PDF файлы.')
+        return photo
+
     class Meta:
         model = Certificate
         fields = ['name', 'givenfor', 'photo']
@@ -10,5 +19,5 @@ class CertificateForm(ModelForm):
         widgets = {
             'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Название'}),
             'givenfor': TextInput(attrs={'class': 'form-control', 'placeholder': 'Выдана за'}),
-            'photo': FileInput(attrs={'class': 'form-control', 'placeholder': 'Photo', 'accept': 'image/*'}),
+            'photo': FileInput(attrs={'class': 'form-control', 'placeholder': 'Файл', 'accept': '.pdf,.jpg,.jpeg,.png,.gif'}),
         }
